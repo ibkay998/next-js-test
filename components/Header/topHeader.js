6232a996a6a169187ea6df53b1a3b96365b1bb72
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2021-07-19 23:24:03 +0100

    added navbar complete

--- components/Header/topHeader.js
+++ components/Header/topHeader.js
@@ -1,5 +1,7 @@
 import { Box } from "@chakra-ui/react"
 import { Grid, GridItem } from "@chakra-ui/react"
+import styles from "../layout.module.css"
+import { Flex,Avatar, Wrap, WrapItem, AvatarBadge, AvatarGroup } from "@chakra-ui/react"
 import {
     Breadcrumb,
     BreadcrumbItem,
@@ -19,40 +21,39 @@ export const siteTitle = 'Next.js Sample Website'
 
 export default function Navbar({ children, user,home}) {
   return (
-    <div >
-       <Box w="100%" bg="teal.500" h="30px">
-       <Breadcrumb float="right">
-            <BreadcrumbItem>
-                <BreadcrumbLink href="/">Home</BreadcrumbLink>
-            </BreadcrumbItem>
+       <Flex  className={styles.boxna}>
+           <Wrap float="left">
+                <WrapItem>
+                    <Avatar name="pi survey" src="public\images\profile.jpg" alt="couldn't find image"/>
+                </WrapItem>
+           </Wrap>
+           <p className={styles.logoname}>PISURV</p>
+           
+        <Breadcrumb className={styles.nav}>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="/">Home</BreadcrumbLink>
+                </BreadcrumbItem>
 
-            <BreadcrumbItem>
-                <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
-            </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href="/dashboard">Dashboard</BreadcrumbLink>
+                </BreadcrumbItem>
 
-            <BreadcrumbItem >
-                <BreadcrumbLink href="/aboutus">About us</BreadcrumbLink>
-            </BreadcrumbItem>
+                <BreadcrumbItem >
+                    <BreadcrumbLink href="/aboutus">About us</BreadcrumbLink>
+                </BreadcrumbItem>
+                
+                <BreadcrumbItem >
+                <BreadcrumbLink action={isloggedout}>
+                    {!user ? (
+                    <p>Login</p> )
+                    : <p>Logout</p>
+
+                    }
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
             
-            <BreadcrumbItem >
-            <BreadcrumbLink action={isloggedout}>
-                {!user ? (
-                <p>Login</p> )
-                : <p>Logout</p>
+            </Breadcrumb>
+       </Flex>
 
-                }
-                </BreadcrumbLink>
-            </BreadcrumbItem>
-          
-        </Breadcrumb>
-       </Box>
-      {!home && (
-        <div >
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -27,13 +27,10 @@ export default function Home({ allPostsData }) {
 
   return (
     <Layout home>
-      
       {/* Keep the existing code here */}
 
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        
-        <h2 className={utilStyles.headingLg}>Blog</h2>
         <WriteToCloudFiresore/>
         <Link href={`/posts/login`}>
           <a>Login</a>
