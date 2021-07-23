cd192c998690f2dcd197d5df3e1f60241b0ec7f5
Beesama <gbozee@gmail.com>
2021-07-24 00:26:07 +0100

    started landing page

--- components/Header/python.py
+++ components/Header/python.py
@@ -1,32 +1,15 @@
-def findout(c):
-    count = 0
-    number = 0
-    if c[0]==0:
-        c.pop(0)
-        
-        for j,i in enumerate(c):
-            try:
-                if i==0:
-                    count+=1
-                    number+=1
-                
-                if i==1 and c[j-1]==0:
-                    count+=1
-                    number=0
-                
-                        
-                if i == 1:
-                    number=0
+import numpy as np
 
-                if number == 2:
-                    count-=1
-                    number=0
-               
-                
-            except IndexError:
-                pass
-        if c[-1]==1:
-            count-=1
-    return count ,c
+def findout():
+    data=np.array([[1 ,1 ,1 ,0 ,0 ,0],
+               [0, 1, 0, 0, 0, 0],
+               [1, 1, 1, 0, 0, 0],
+               [0 ,0, 2, 4, 4, 0],
+               [0 ,0 ,0 ,2 ,0 ,0],
+               [0, 0, 1, 2, 4, 0]])
+    
+
+    return data
+    
         
-print(findout([0,0,0,0,0,0,0]))
+print(findout())
--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,26 @@ import Link from 'next/link'
 import Date from '../components/date'
 import firebase from '../firebase/firebase'
 import WriteToCloudFiresore from '../firebase/firestore'
+import {
+  Image,
+  Text,
+  Box,
+  Spacer,
+  Grid,
+  GridItem,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+  Button,
+  Circle,
+} from "@chakra-ui/react";
 
 
 firebase()
 
 import { getSortedPostsData } from '../lib/posts'
+import { Flex } from '@chakra-ui/layout'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -26,29 +41,73 @@ export default function Home({ allPostsData }) {
   
 
   return (
-    <Layout home>
-      {/* Keep the existing code here */}
+    <Flex className="homepage" display="flex" flexDir="column">
+      <Box w="100%" h="50px" display="flex" flexDir="row">
+        <Text
+          mt={[6]}
+          ml={[6]}
+          color="white"
+          fontFamily="Roboto"
+          fontSize="24px"
+          fontWeight="bold"
+          flexWrap="wrap"
+        >
+          PiSurv
+        </Text>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <BreadcrumbLink>Home</BreadcrumbLink>
+            <BreadcrumbLink>Surveys</BreadcrumbLink>
+            <BreadcrumbLink>About</BreadcrumbLink>
+            <BreadcrumbLink>Sign In</BreadcrumbLink>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </Box>
+      <Box>
+        <Box w={["30%"]}>
+          <Text>Imagine no more </Text>
+          <Text
+            fontFamily="Roboto"
+            color="white"
+            fontSize="59px"
+            borderBottom="solid"
+            borderBottomColor="white"
+            pb="4px"
+          >
+            Pisurv
+          </Text>
+          <Text  color="white">
+            A platform where companies meet users to get surveys filled
+          </Text>
+          <Button> Get Started</Button>
+          <Image className="fancyimage" color="white" display="block" src="rightsection.png" />
+        </Box>
+      </Box>
+      <Box></Box>
+    </Flex>
+    // <Layout home>
+    //   {/* Keep the existing code here */}
 
-      {/* Add this <section> tag below the existing <section> tag */}
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <WriteToCloudFiresore/>
-        <Link href={`/posts/login`}>
-          <a>Login</a>
-        </Link>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
+    //   {/* Add this <section> tag below the existing <section> tag */}
+    //   {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+    //     <WriteToCloudFiresore/>
+    //     <Link href={`/posts/login`}>
+    //       <a>Login</a>
+    //     </Link>
+    //     <ul className={utilStyles.list}>
+    //       {allPostsData.map(({ id, date, title }) => (
+    //         <li className={utilStyles.listItem} key={id}>
+    //         <Link href={`/posts/${id}`}>
+    //           <a>{title}</a>
+    //         </Link>
+    //         <br />
+    //         <small className={utilStyles.lightText}>
+    //           <Date dateString={date} />
+    //         </small>
+    //       </li>
+    //       ))}
+    //     </ul>
+    //   </section> */}
+    // </Layout>
+  );
 }
